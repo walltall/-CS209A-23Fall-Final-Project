471bd7792af0062c9efc7515b844b3523891af63
walltall <3077161150@qq.com>
2023-12-15 14:56:22 +0800

    wu

---
+++ src/main/java/org/example/java2finalproject/Java2FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.java2finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java2FinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java2FinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/java2finalproject/Java2FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.java2finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java2FinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
